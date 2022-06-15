994f7bbf0abd94f0c0e502d1c24afb01aceb3ef4
mshannon-platoon <platoon.development@gmail.com>
2022-06-15 21:40:20 +0100

    initial commit

---
+++ src/main/java/ie/plat/identity/PlatIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.plat.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/plat/identity/PlatIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.plat.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
