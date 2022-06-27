14feaee68d4856aa17c321e3cd62866117e92b57
mshannon-platoon <platoon.development@gmail.com>
2022-06-27 12:52:31 +0100

    add initial endpoint structure and validators

---
+++ src/main/java/ie/plat/identity/registration/model/UserDTO.java
@@ -0,0 +1,30 @@
+package ie.plat.identity.registration.model;
+
+import ie.plat.identity.registration.validation.ValidEmail;
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserDTO {
+
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    @ValidEmail
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+}
---
+++ src/main/java/ie/plat/identity/registration/validation/EmailValidator.java
@@ -0,0 +1,30 @@
+package ie.plat.identity.registration.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+
+    private Matcher matcher;
+
+    private Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,9}$");
+
+    @Override
+    public void initialize(ValidEmail constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return (validateEmail(email));
+    }
+
+    private boolean validateEmail(String email) {
+        matcher = emailPattern.matcher(email);
+        return matcher.matches();
+    }
+}
